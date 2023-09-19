206938fd77ae28776bc898da8a0c31a9ebf3a9e1
Ian Nelson <ian.gregory.nelson@gmail.com>
2023-09-18 20:09:27 -0500

    room area calculator

---
+++ small_problems/easy_2/sq_meters.rb
@@ -0,0 +1,9 @@
+# square meters
+
+puts "Room length in meters please: "
+length = gets.chomp.to_i
+
+puts "Room width in meters: "
+width = gets. chomp.to_i
+
+puts "The area of the room in meters is #{length * width} square meters and #{(length*width*10.7693).round(2)} square feet."
