632a87be1229dc757a4a77683653fffc11c6d550
Ian Nelson <ian.gregory.nelson@gmail.com>
2023-09-18 20:18:34 -0500

    Tip calculator

---
+++ small_problems/easy_2/tip_calculator.rb
@@ -0,0 +1,11 @@
+# tip calculator
+
+p "What is the bill? "
+bill = gets.chomp.to_f
+p "What is the tip percentage? "
+tip_percent = gets.chomp.to_f
+tip = (bill * (tip_percent/100)).round(2)
+total = (bill + tip).round(2)
+
+puts "The tip is $#{tip}."
+puts "The total is $#{total}."
