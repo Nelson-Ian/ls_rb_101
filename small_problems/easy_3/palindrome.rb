ea53749dbee474dcd29070e09cd0fead6929a389
Ian Nelson <ian.gregory.nelson@gmail.com>
2023-09-20 18:44:52 -0500

    Palindrome update with real_palidrome

--- small_problems/easy_3/palindrome.rb
+++ small_problems/easy_3/palindrome.rb
@@ -4,6 +4,14 @@ def palindrome?(text)
   text == text.reverse
 end
 
+puts "Palidrome"
+puts palindrome?('madam') == true
+puts palindrome?('Madam') == false          # (case matters)
+puts palindrome?("madam i'm adam") == false # (all characters matter)
+puts palindrome?('356653') == true
+
+
+
 def arr_palindrome?(input)
   original = []
   0.upto(input.size-1) do |i|
@@ -12,11 +20,20 @@ def arr_palindrome?(input)
   palindrome?(original.join)
 end
 
-
-puts palindrome?('madam') == true
-puts palindrome?('Madam') == false          # (case matters)
-puts palindrome?("madam i'm adam") == false # (all characters matter)
-puts palindrome?('356653') == true
-
+puts "arr_palindrome"
 puts arr_palindrome?([1,2,3,2,1]) == true
 puts arr_palindrome?('madam madam') == true
+
+
+
+def real_palindrome?(input)
+  palindrome?(input.to_s.downcase.delete('^a-z0-9'))
+end
+
+puts "real_palidrome"
+puts real_palindrome?('madam') == true
+puts real_palindrome?('Madam') == true           # (case does not matter)
+puts real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+puts real_palindrome?('356653') == true
+puts real_palindrome?('356a653') == true
+puts real_palindrome?('123ab321') == false
