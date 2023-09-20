4531eb0c133f1c10afc082c736c2de04f9605313
Ian Nelson <ian.gregory.nelson@gmail.com>
2023-09-20 11:03:37 -0500

    Palindrome checker for strings and/or arrays

--- small_problems/easy_3/palindrome.rb
+++ small_problems/easy_3/palindrome.rb
@@ -4,7 +4,19 @@ def palindrome?(text)
   text == text.reverse
 end
 
+def arr_palindrome?(input)
+  original = []
+  0.upto(input.size-1) do |i|
+    original << input[i]
+  end
+  palindrome?(original.join)
+end
+
+
 puts palindrome?('madam') == true
 puts palindrome?('Madam') == false          # (case matters)
 puts palindrome?("madam i'm adam") == false # (all characters matter)
 puts palindrome?('356653') == true
+
+puts arr_palindrome?([1,2,3,2,1]) == true
+puts arr_palindrome?('madam madam') == true
