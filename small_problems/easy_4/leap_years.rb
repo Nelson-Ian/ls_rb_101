b5e77c2f2eb8f8d7b96558a7976c2787f02ca981
Ian Nelson <ian.gregory.nelson@gmail.com>
2023-09-21 11:37:05 -0500

    Adding gregorian/julian calendar differentiator

--- small_problems/easy_4/leap_years.rb
+++ small_problems/easy_4/leap_years.rb
@@ -21,3 +21,32 @@ puts leap_year?(1700) == false
 puts leap_year?(1) == false
 puts leap_year?(100) == false
 puts leap_year?(400) == true
+
+
+def julian?(year)
+  year % 4 == 0 
+end
+
+def gregorian?(year)
+  leap_year?(year)
+end
+
+def julian_gregorian_leap_years?(year)
+  year <= 1752 ? julian?(year) : gregorian?(year)
+end
+
+puts "epoch comparison"
+puts julian_gregorian_leap_years?(2016) == true
+puts julian_gregorian_leap_years?(2016) == true
+puts julian_gregorian_leap_years?(2015) == false
+puts julian_gregorian_leap_years?(2100) == false
+puts julian_gregorian_leap_years?(2400) == true
+puts julian_gregorian_leap_years?(240000) == true
+puts julian_gregorian_leap_years?(240001) == false
+puts julian_gregorian_leap_years?(2000) == true
+puts julian_gregorian_leap_years?(1900) == false
+puts julian_gregorian_leap_years?(1752) == true
+puts julian_gregorian_leap_years?(1700) == true
+puts julian_gregorian_leap_years?(1) == false
+puts julian_gregorian_leap_years?(100) == true
+puts julian_gregorian_leap_years?(400) == true
